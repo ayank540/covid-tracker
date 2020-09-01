eac098f815dff021e987a1363c6167acd83f8bd8
Ayan Khan <42548565+ayank540@users.noreply.github.com>
2020-09-01 16:07:31 +0530

    Added Chart component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from './Components/Cards/Cards'
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
+    return ( 
+        <div >
+            <Cards />
         </div>
     )
 }
