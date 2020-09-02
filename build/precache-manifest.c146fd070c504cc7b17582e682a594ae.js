79d0436127ebf217e46a86a8c60f67b79e00705c
Ayan Khan <42548565+ayank540@users.noreply.github.com>
2020-09-02 11:06:46 +0530

    Build ready

---
+++ build/precache-manifest.c146fd070c504cc7b17582e682a594ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28efd56ae39bff688f45696bb16656d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5930732ed01cce332aa9",
+    "url": "/static/css/main.5d5d587d.chunk.css"
+  },
+  {
+    "revision": "680d09427fb2bacb0c88",
+    "url": "/static/js/2.ea45331a.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.ea45331a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5930732ed01cce332aa9",
+    "url": "/static/js/main.84044e3f.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  },
+  {
+    "revision": "310e8b538cdd753f9be60e20413d780c",
+    "url": "/static/media/image.310e8b53.jpg"
+  }
+]);
